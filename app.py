4d0345ac28c38f1bb158a891f802db0ebc730997
Robin Kaal <rkaal7@gmail.com>
2019-12-23 13:30:41 +0100

    Version 1.20 - Created new database connection.

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ import bcrypt
 
 app = Flask(__name__)
 
+# CONNECTION TO DATABASE
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
 mongo = PyMongo(app)
 
 # WEB PAGES
